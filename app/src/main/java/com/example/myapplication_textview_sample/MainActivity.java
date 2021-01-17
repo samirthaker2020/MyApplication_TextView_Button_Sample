a9a00fb53db1600a22d86eb7e706d4c13552f477
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-17 17:09:41 -0500

    declare variables, fetched control ids

--- app/src/main/java/com/example/myapplication_textview_sample/MainActivity.java
+++ app/src/main/java/com/example/myapplication_textview_sample/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.myapplication_textview_sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnclick;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // fetching controls id's
+        btnclick=(Button) findViewById(R.id.btn_click);
+        result=(TextView) findViewById(R.id.textView_result);
+
+
+        //on click display text
+        btnclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
